527ec1b7c8110ef3532fa3845c376360ff0ccdfb
deleu <deleuzethomasbisson@gmail.com>
2025-10-31 20:53:05 -0400

    Fix: add className prop support to Logo

--- components/ui/logo.tsx
+++ components/ui/logo.tsx
@@ -1,11 +1,24 @@
+"use client";
+
 import Link from "next/link";
 import Image from "next/image";
 import logo from "@/public/images/logo.svg";
 
-export default function Logo() {
+interface LogoProps {
+  className?: string;
+}
+
+export default function Logo({ className = "" }: LogoProps) {
   return (
-    <Link href="/" className="inline-flex shrink-0" aria-label="Cruip">
-      <Image src={logo} alt="Cruip Logo" width={32} height={32} />
+    <Link href="/" className={`inline-flex shrink-0 ${className}`} aria-label="Cruip">
+      <Image
+        src={logo}
+        alt="Cruip Logo"
+        width={32}
+        height={32}
+        className="w-full h-full object-cover rounded-full"
+        priority
+      />
     </Link>
   );
 }
